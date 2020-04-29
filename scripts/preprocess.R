777514ec726edbef19ccd76538d4c404fd0e5e33
Marcel Ribeiro-Dantas <marcel.ribeiro-dantas@curie.fr>
2020-04-29 03:14:56 +0200

    Updates preprocessing pipeline with HK/TK/Réunion
    Changes country_name to locality_name (Réunion and Hong Kong are not countries,
    for example).
    
    Réunion and Hong Kong are not in the ECDC dataset for COVID-19. Brings
    these numbers from John Hopkins University dataset.
    
    Updates population data from World Bank for Hong Kong and Eurostats for
    Réunion.
    
    Explains warnings from min(date[acc_cases > 0])

--- scripts/preprocess.R
+++ scripts/preprocess.R
@@ -15,7 +15,7 @@ raw_dataset <- read_csv(file = 'data/raw/Global_Mobility_Report.csv',
                                                 rep('d', 6)),
                                               collapse=''))
 
-colnames(raw_dataset) <- c('country_code', 'country_name', 'region_name',
+colnames(raw_dataset) <- c('country_code', 'locality_name', 'region_name',
                            'county_name', 'date', 'retail_recreation',
                            'grocery_pharmacy', 'parks', 'transit_stations',
                            'workplaces', 'residential')
@@ -28,8 +28,14 @@ covid <- read_delim(file = 'data/raw/COVID19_worldwide_raw.csv', na = '',
                     delim = ',')
 
 colnames(covid) <- c('date', 'day', 'month', 'year', 'new_cases', 'new_deaths',
-                     'country_name', 'country_id', 'territory_id',
+                     'locality_name', 'country_id', 'territory_id',
                      'pop_data_2018')
+
+# COVID-19 data for Réunion and Hong Kong  that are missing in the ECDC dataset
+# Source: John Hopkins University https://github.com/CSSEGISandData/COVID-19
+covid_hk_re <- read_delim(file = 'data/raw/hk-reunion-covid-19.csv',
+                          col_types = cols('c', 'c', 'd', 'd'),
+                          delim = ',')
 
 # Country details from UN Data
 country_details <- read_delim(file = 'data/raw/UN_dataset.tsv', delim = '\t',
@@ -63,30 +69,49 @@ covid %>%
   # Convert date to Date
   mutate(date, date = dmy(covid$date)) %>%
   # Replace _ by space in country names
-  mutate(country_name,
-         country_name = gsub('_',
+  mutate(locality_name,
+         locality_name = gsub('_',
                         ' ',
-                        country_name)) -> covid
+                        locality_name)) -> covid
+
+covid_hk_re %>%
+  # Convert date to Date
+  mutate(date, date = mdy(covid_hk_re$date)) -> covid_hk_re
+
+covid_hk_re$pop_data_2018 <- NA
+covid <- rbind(covid, covid_hk_re)
+
+# Adds pop data
+covid %>%
+  # From World Bank https://data.worldbank.org/indicator/sp.pop.totl
+  mutate(pop_data_2018 = ifelse(locality_name == 'Hong Kong',
+                               7451000, pop_data_2018)) %>%
+  # From eurostats
+  # https://pt.wikipedia.org/wiki/Reunião_(departamento)#cite_note-GDP-2
+  mutate(pop_data_2018 = ifelse(locality_name == 'Réunion',
+                               865826, pop_data_2018)) -> covid
+
+rm(covid_hk_re)
 
 # Some countries have different names between the datasets
 covid %>%
-  mutate(country_name, country_name = case_when(
-    country_name == 'United States of America' ~ 'United States',
-    country_name == 'United Republic of Tanzania' ~ 'Tanzania',
-    country_name == 'Guinea Bissau' ~ 'Guinea-Bissau',
-    country_name == 'Bahamas' ~ 'The Bahamas',
-    country_name == 'Myanmar' ~ 'Myanmar (Burma)',
-    country_name == 'Cote dIvoire' ~ 'Côte d\'Ivoire',
-    TRUE ~ country_name)
+  mutate(locality_name, locality_name = case_when(
+    locality_name == 'United States of America' ~ 'United States',
+    locality_name == 'United Republic of Tanzania' ~ 'Tanzania',
+    locality_name == 'Guinea Bissau' ~ 'Guinea-Bissau',
+    locality_name == 'Bahamas' ~ 'The Bahamas',
+    locality_name == 'Myanmar' ~ 'Myanmar (Burma)',
+    locality_name == 'Cote dIvoire' ~ 'Côte d\'Ivoire',
+    TRUE ~ locality_name)
     ) -> covid
 
 # Some rows are missing like March 2nd and 3rd for Brazil. Apparently,
 # this happens when there is no case/death. Complete with these rows.
 covid %>%
-  complete(date = seq.Date(min(date), max(date), by='day'), country_name) %>%
+  complete(date = seq.Date(min(date), max(date), by='day'), locality_name) %>%
   mutate(new_cases = ifelse(is.na(new_cases), 0, new_cases)) %>%
   mutate(new_deaths = ifelse(is.na(new_deaths), 0, new_deaths)) %>%
-  group_by(country_name) %>%
+  group_by(locality_name) %>%
   fill(pop_data_2018, .direction = 'updown') -> covid
 
 ####
@@ -96,7 +121,7 @@ covid %>%
 ####
 
 preprocessed_dataset <- left_join(preprocessed_dataset, covid,
-                                  by = c('date', 'country_name'))
+                                  by = c('date', 'locality_name'))
 rm(covid)
 
 ####
@@ -105,9 +130,20 @@ rm(covid)
 #
 ####
 
+# GMR has data on Tajikistan. Updates info
+# Tajikistan has not had any case related to COVID-19 so far.
+# https://reliefweb.int/report/tajikistan/covid-19-tajikistan-situation-report-4-27-april-2020
+preprocessed_dataset %>%
+  mutate(new_cases = ifelse(locality_name == 'Tajikistan',
+                            0, new_cases)) %>%
+  mutate(new_deaths = ifelse(locality_name == 'Tajikistan',
+                             0, new_deaths)) %>%
+  mutate(pop_data_2018 = ifelse(locality_name == 'Tajikistan',
+                                9100000, pop_data_2018)) -> preprocessed_dataset
+
 # Create column with accumulate cases/death
 preprocessed_dataset %>%
-  group_by(country_name, plot_name, region_name) %>%
+  group_by(locality_name, plot_name, region_name) %>%
   arrange(date) %>%
   dplyr::mutate(acc_cases = cumsum(new_cases)) %>%
   dplyr::mutate(acc_deaths = cumsum(new_deaths)) %>%
@@ -131,8 +167,8 @@ preprocessed_dataset %>%
 
 # Before merging to get more info about the countries, we must make sure all
 # country names are the same.
-#unique(preprocessed_dataset$country_name)[which(
-#     unique(preprocessed_dataset$country_name) %in%
+#unique(preprocessed_dataset$locality_name)[which(
+#     unique(preprocessed_dataset$locality_name) %in%
 #       unique(country_details$region_name) == FALSE
 #   )
 # ]
@@ -183,7 +219,7 @@ country_details <- country_details[-217, ]
 ####
 
 preprocessed_dataset <- left_join(preprocessed_dataset, country_details,
-                                  by = c('country_name' = 'region_name'))
+                                  by = c('locality_name' = 'region_name'))
 
 rm(country_details)
 
@@ -203,7 +239,7 @@ colnames(preprocessed_dataset)[10:183] %>%
 
 # Create columns for lethality rate
 preprocessed_dataset %>%
-  group_by(country_name) %>%
+  group_by(locality_name) %>%
   arrange(date) %>%
   mutate(lethality_rate_percent = (acc_deaths[n()]/acc_cases[n()])*100) %>%
   ungroup() -> preprocessed_dataset
@@ -214,23 +250,41 @@ preprocessed_dataset %>%
 
 # Add n_days_since_1st_case column
 preprocessed_dataset %>%
-  group_by(country_name) %>%
+  group_by(locality_name) %>%
   mutate(first_case_date = min(date[acc_cases > 0])) %>%
   mutate(n_days_since_1st_case =
            if_else(acc_cases > 0,
-                   as.numeric(date - min(date[acc_cases > 0])+1),
+                   as.numeric(date - min(date[acc_cases > 0])),
                    0)) %>%
   ungroup() -> preprocessed_dataset
+# There is a warning here, because Tajikistan doesn't have any date with
+# cases. So the minimum of nothing will throw a warning.
+# preprocessed_dataset %>%
+#   group_by(locality_name) %>%
+#   arrange(date) %>%
+#   filter(date == '2020-04-17') %>%
+#   filter(acc_cases == 0) %>%
+#   select(locality_name) %>%
+#   unique
 
 # Add n_days_since_1st_death column
 preprocessed_dataset %>%
-  group_by(country_name) %>%
+  group_by(locality_name) %>%
   mutate(first_death_date = min(date[acc_deaths > 0])) %>%
   mutate(n_days_since_1st_death =
            if_else(acc_deaths > 0,
-                   as.numeric(date - min(date[acc_deaths > 0])+1),
+                   as.numeric(date - min(date[acc_deaths > 0])),
                    0)) %>%
   ungroup() -> preprocessed_dataset
+# There is a warning here, because 15 countries don't have any date with
+# deaths. So the minimum of nothing will throw a warning.
+# preprocessed_dataset %>%
+#   group_by(locality_name) %>%
+#   arrange(date) %>%
+#   filter(date == '2020-04-17') %>%
+#   filter(acc_deaths == 0) %>%
+#   select(locality_name) %>%
+#   unique
 
 # Set manually first case for countries whose first case happened before Feb 15
 # Wikipedia contributors, "2019–20 coronavirus pandemic", Wikipedia, The Free
@@ -239,33 +293,33 @@ preprocessed_dataset %>%
 # (accessed April 21, 2020).
 preprocessed_dataset %>%
   mutate(first_case_date = case_when(
-    # country_name == 'China' ~ dmy('01-12-2019'),
-    country_name == 'Thailand' ~ dmy('13-01-2020'),
-    country_name == 'Japan' ~ dmy('16-01-2020'),
-    country_name == 'South Korea' ~ dmy('20-01-2020'),
-    country_name == 'United States' ~ dmy('20-01-2020'),
-    country_name == 'Taiwan' ~ dmy('21-01-2020'),
-    country_name == 'Hong Kong' ~ dmy('22-01-2020'),
-    country_name == 'Singapore' ~ dmy('23-01-2020'),
-    country_name == 'Vietnam' ~ dmy('23-01-2020'),
-    country_name == 'France' ~ dmy('24-01-2020'),
-    country_name == 'Nepal' ~ dmy('24-01-2020'),
-    country_name == 'Australia' ~ dmy('25-01-2020'),
-    country_name == 'Canada' ~ dmy('25-01-2020'),
-    country_name == 'Malaysia' ~ dmy('25-01-2020'),
-    country_name == 'Cambodia' ~ dmy('27-01-2020'),
-    country_name == 'Germany' ~ dmy('27-01-2020'),
-    country_name == 'Sri Lanka' ~ dmy('27-01-2020'),
-    country_name == 'Finalnd' ~ dmy('29-01-2020'),
-    country_name == 'United Arab Emirates' ~ dmy('29-01-2020'),
-    country_name == 'India' ~ dmy('30-01-2020'),
-    country_name == 'Italy' ~ dmy('30-01-2020'),
-    country_name == 'Philippines' ~ dmy('30-01-2020'),
-    country_name == 'Spain' ~ dmy('31-01-2020'),
-    country_name == 'Sweden' ~ dmy('31-01-2020'),
-    country_name == 'United Kingdom' ~ dmy('31-01-2020'),
-    country_name == 'Belgium' ~ dmy('04-02-2020'),
-    country_name == 'Egypt' ~ dmy('14-01-2020'),
+    # locality_name == 'China' ~ dmy('01-12-2019'),
+    locality_name == 'Thailand' ~ dmy('13-01-2020'),
+    locality_name == 'Japan' ~ dmy('16-01-2020'),
+    locality_name == 'South Korea' ~ dmy('20-01-2020'),
+    locality_name == 'United States' ~ dmy('20-01-2020'),
+    locality_name == 'Taiwan' ~ dmy('21-01-2020'),
+    locality_name == 'Hong Kong' ~ dmy('22-01-2020'),
+    locality_name == 'Singapore' ~ dmy('23-01-2020'),
+    locality_name == 'Vietnam' ~ dmy('23-01-2020'),
+    locality_name == 'France' ~ dmy('24-01-2020'),
+    locality_name == 'Nepal' ~ dmy('24-01-2020'),
+    locality_name == 'Australia' ~ dmy('25-01-2020'),
+    locality_name == 'Canada' ~ dmy('25-01-2020'),
+    locality_name == 'Malaysia' ~ dmy('25-01-2020'),
+    locality_name == 'Cambodia' ~ dmy('27-01-2020'),
+    locality_name == 'Germany' ~ dmy('27-01-2020'),
+    locality_name == 'Sri Lanka' ~ dmy('27-01-2020'),
+    locality_name == 'Finalnd' ~ dmy('29-01-2020'),
+    locality_name == 'United Arab Emirates' ~ dmy('29-01-2020'),
+    locality_name == 'India' ~ dmy('30-01-2020'),
+    locality_name == 'Italy' ~ dmy('30-01-2020'),
+    locality_name == 'Philippines' ~ dmy('30-01-2020'),
+    locality_name == 'Spain' ~ dmy('31-01-2020'),
+    locality_name == 'Sweden' ~ dmy('31-01-2020'),
+    locality_name == 'United Kingdom' ~ dmy('31-01-2020'),
+    locality_name == 'Belgium' ~ dmy('04-02-2020'),
+    locality_name == 'Egypt' ~ dmy('14-01-2020'),
     TRUE ~ first_case_date
     )
   ) -> preprocessed_dataset
@@ -278,9 +332,9 @@ countries <- c('Thailand', 'Japan', 'South Korea', 'United States', 'Taiwan',
                'Italy', 'Philippines', 'Spain', 'Sweden', 'United Kingdom',
                'Belgium', 'Egypt')
 preprocessed_dataset %>%
-  group_by(country_name) %>%
+  group_by(locality_name) %>%
   mutate(n_days_since_1st_case =
-           if_else(country_name %in% countries,
+           if_else(locality_name %in% countries,
                    as.numeric(date - first_case_date)+1,
                    n_days_since_1st_case)) %>%
   ungroup() -> preprocessed_dataset
