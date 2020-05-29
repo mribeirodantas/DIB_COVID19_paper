fdae416f28a5337cc84f8f253c9ffdabfc83a730
Marcel Ribeiro-Dantas <marcel.ribeiro-dantas@curie.fr>
2020-05-29 22:07:19 +0200

    Fix locality name varialbe

--- extra/compare_countries.R
+++ extra/compare_countries.R
@@ -8,14 +8,14 @@ df <- read_delim(file = 'data/preprocessed/DIB_dataset.tsv', delim = '\t')
 # What countries have more than 100 confirmed deaths by COVID-19?
 df %>%
   filter(acc_deaths > 100) %>%
-  group_by(country_name) %>%
-  select(country_name, acc_cases) %>%
+  group_by(locality_name) %>%
+  select(locality_name, acc_cases) %>%
   slice(n()) %>%
   arrange(-acc_cases) %>% View
 
 # What are the top 10 countries by lethality rate?
 df %>%
-  select(country_name, lethality_rate_percent) %>%
+  select(locality_name, lethality_rate_percent) %>%
   unique %>%
   arrange(-lethality_rate_percent) %>%
   slice(1:10) %>%
